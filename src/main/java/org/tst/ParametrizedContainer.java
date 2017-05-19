7c94be73d2222a0abe46ce0939375c60b164a35a
Guilherme Moreira <guilherme.moreira@bb.com.br>
2017-05-19 08:16:26 -0300

    anotacoes jaxb

--- src/main/java/org/tst/ParametrizedContainer.java
+++ src/main/java/org/tst/ParametrizedContainer.java
@@ -1,7 +1,13 @@
 package org.tst;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlAccessorType(XmlAccessType.PROPERTY)
 public class ParametrizedContainer<T> {
-	
+
+	@XmlElement
 	private T data;
 
 	public T getData() {
